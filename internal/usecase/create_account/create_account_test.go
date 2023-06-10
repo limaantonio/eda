5f583a00ce48e95c9376c4c701439ec18643147e
DESKTOP-L4IT8DB\Antonio <limaantoniocarlos20@gmail.com>
2023-06-09 23:32:42 -0300

    first commit

---
+++ internal/usecase/create_account/create_account_test.go
@@ -0,0 +1,60 @@
+package create_account
+
+import (
+	"testing"
+
+	"github.com.br/limaantonio/ms-wallet/internal/entity"
+	"github.com.br/limaantonio/ms-wallet/internal/usecase/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type ClientGatewayMock struct {
+	mock.Mock
+}
+
+func (m *ClientGatewayMock) Save(client *entity.Client) error {
+	args := m.Called(client)
+	return args.Error(0)
+}
+
+func (m *ClientGatewayMock) Get(id string) (*entity.Client, error) {
+	args := m.Called(id)
+	return args.Get(0).(*entity.Client), args.Error(1)
+}
+
+type AccountGatewayMock struct {
+	mock.Mock
+}
+
+func (m *AccountGatewayMock) Save(account *entity.Account) error {
+	args := m.Called(account)
+	return args.Error(0)
+}
+
+func (m *AccountGatewayMock) FindById(id string) (*entity.Account, error) {
+	args := m.Called(id)
+	return args.Get(0).(*entity.Account), args.Error(1)
+}
+
+func TestCreateAccountUseCase_Execute(t *testing.T) {
+	client, _ := entity.NewClient("John Doe", "j@j")
+	clientMock := &mocks.ClientGatewayMock{}
+	clientMock.On("Get", client.ID).Return(client, nil)
+
+	accountMock := &mocks.AccountGatewayMock{}
+	accountMock.On("Save", mock.Anything).Return(nil)
+
+	uc := NewCreateAccountUseCase(accountMock, clientMock)
+	inputDto := CreateAccountInputDTO{
+		ClientId: client.ID,
+	}
+	output, err := uc.Execute(inputDto)
+	assert.Nil(t, err)
+	assert.NotNil(t, output.Id)
+	// asssert valid uuid
+	clientMock.AssertExpectations(t)
+	accountMock.AssertExpectations(t)
+	clientMock.AssertNumberOfCalls(t, "Get", 1)
+	accountMock.AssertNumberOfCalls(t, "Save", 1)
+}
